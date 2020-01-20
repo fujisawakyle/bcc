a2366de315fb2647c865006301a96d4d32f3e2b9
fujisawakyle <fujisawakyle@gmail.com>
2020-01-20 11:20:44 -0800

    tweak picture size

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-highlights/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Tessa + Kyle',
-  subHeading: 'Better Chicken Commitment',
+  subHeading: 'Beautiful Commitment Celebration',
   date: 'September, 2020',
   // social
   socialLinks: [
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
       <header className="major">
         <h1 style={{ textShadow: "2.5px 2px #b8567c" }}>{config.heading}</h1>
         <br />
-        <img style={{ maxWidth: "500px", width: "100%", opacity: "0.8" }} src={TessKylePic} />
+        <img style={{ maxWidth: "350px", width: "100%", opacity: "0.8", maxHeight: "500px" }} src={TessKylePic} />
         <p style={{ fontSize: '2.7em', marginTop: '1em', fontFamily: "'Dancing Script', cursive", textShadow: "2.5px 2px #b8567c", lineHeight: 1 }}>{config.subHeading}</p>
         <p style={{ fontSize: '1.5em', marginTop: '0.7em', textShadow: "2.5px 2px #b8567c" }}>{config.date}</p>
       </header>
